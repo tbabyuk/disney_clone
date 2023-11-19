be74bbb7b50d8656fca2eaa32d28f255e906f8ea
Terry Babyuk <tbabyuk@gmail.com>
2023-11-19 02:25:14 -0500

    initial commit, top header and nav completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        disney_white: "FFFFFF",
+        disney_gray: "F5F5F5",
+      },
     },
   },
   plugins: [],
