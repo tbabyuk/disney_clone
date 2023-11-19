5d013370a0b7bd640c75bc8057a77cc4fcfab8df
Terry Babyuk <tbabyuk@gmail.com>
2023-11-19 17:42:45 -0500

    finished sections 2 and 3

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 import { IoPlay } from "react-icons/io5";
 
+// components
+
+import { Card1 } from './components/Card1';
+import { TrendingCard } from './components/TrendingCard';
 
 
 export default function Home() {
@@ -37,6 +41,20 @@ export default function Home() {
           <button className="border w-[240px] mt-3 py-3 rounded-3xl text-[0.95rem] text-white font-semibold flex justify-center items-center hover:bg-gray-500 hover:bg-opacity-50 hover:border-gray-500 hover:border-opacity-50"><IoPlay className="me-2" size="17px" />Watch Trailer</button>
         </div>
       </div>
+
+      <section className="section-2 py-12 flex justify-center flex-wrap gap-5 bg-[#F1F2F3]">
+        <Card1 imageUrl="/images/section_2_image_1.jpeg" title="Wish" description="Disney's Wish is Coming Only to Theaters November 22" link="GET TICKETS NOW" />
+        <Card1 imageUrl="/images/section_2_image_2.jpeg" title="New on Disney+" description="Dashing Through the Snow Is Now Streaming" link="STREAM NOW" />
+        <Card1 imageUrl="/images/section_2_image_3.jpeg" title="shopDisney" description="Celebrate 10 Years of Frozen with Dolls, Apparel, and More!" link="SHOP NOW" />
+        <Card1 imageUrl="/images/section_2_image_4.jpeg" title="Adventures by Disney&reg;" description="Embark on Winter Adventures with Savings of Up to $350 per Person on Select Land Adventures Departures" link="LEARN MORE" />
+      </section>
+      <section className="trending-section bg-[#0A193C] py-8">
+          <h2 className="text-[1.8rem] text-white text-center pb-8">Trending on Disney+</h2>
+          <div className="flex flex-wrap justify-center gap-5">
+            <TrendingCard imageUrl="/images/trending_image_1.jpeg" title="The Santa Clauses" description="Season 2 of the Disney+ Original series is now streaming!" link="STREAM NOW" />
+            <TrendingCard imageUrl="/images/trending_image_2.jpeg" title="Marvel Studios' Loki" description="Stream Season 2 now on Disney+." link="STREAM NOW" />
+          </div>
+      </section>
     </main>
   )
 }
