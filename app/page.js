749f55dcdf97516fcdede40674a6fe32773eb067
Terry Babyuk <tbabyuk@gmail.com>
2023-11-22 16:08:28 -0500

    finished footer section, desktop version

--- app/page.js
+++ app/page.js
@@ -108,8 +108,26 @@ export default function Home() {
           </div>
       </section>
 
-      <section className="footer-section py-5 h-[200px] bg-[#FFFFFF]">
+      <section className="footer-section py-10 bg-[#FFFFFF]">
             <img src="/images/disney_logo.png" className="mx-auto w-[80px]" />
+            <ul className="flex justify-center text-[0.7rem] mt-5">
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">About Disney</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Disney Help</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Careers</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Contact Us</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Advertise With Us</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Disney&reg; Premier Visa&reg; Card</a></li>
+            </ul>
+            <ul className="flex justify-center text-[0.75rem] font-semibold mt-3">
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Terms of Use</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Additional Content Information</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Privacy Policy</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Your US State Privacy Rights</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Children's Online Privacy Policy</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Do Not Sell or Share My Personal Information</a></li>
+              <li className="me-6"><a href="#" className="hover:text-[#1786EB]">Interest-Based Ads</a></li>
+            </ul>
+            <p className="text-[0.7rem] text-center mt-4">&copy; Disney, All Rights Reserved, Disney Entertainment</p>
       </section>
 
 
